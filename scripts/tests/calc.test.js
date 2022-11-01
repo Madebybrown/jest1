efbfc0902d1ab580469c9439b5cc2e5f11db7671
Madebybrown <madebybrown@gmail.com>
2022-11-01 13:57:13 +0000

    Initial Commit

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,16 @@
+const addition = require("../calc");
+
+describe("calculator", () => {
+    describe("Addition function", () => {
+
+    });
+    describe("Subtraction function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Division function", () => {
+
+    });
+})
